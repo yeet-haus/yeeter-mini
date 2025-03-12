70ec51d37a36e98865804790aba61cfdb4f674f6
skuhlmann <samkuhlmann@gmail.com>
2025-03-12 11:47:49 -0600

    remove extra connect and add logs

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,19 +3,19 @@ import { usePrivy } from "@privy-io/react-auth";
 import User from "../assets/icons/user.svg";
 import LoginIcon from "../assets/icons/login.svg";
 import LogoutIcon from "../assets/icons/logout.svg";
-import WalletIcon from "../assets/icons/id_wallet.svg";
+// import WalletIcon from "../assets/icons/id_wallet.svg";
 
 export const Login = () => {
-  const { ready, authenticated, login, logout, user } = usePrivy();
+  const { ready, authenticated, login, logout } = usePrivy();
 
-  const userNoWallet = user && !user.wallet;
+  // const userNoWallet = user && !user.wallet;
 
   const handleLogin = () => {
-    if (userNoWallet) {
-      login({ loginMethods: ["email", "wallet"] });
-    } else {
-      login();
-    }
+    // if (userNoWallet) {
+    //   login({ loginMethods: ["email", "wallet"] });
+    // } else {
+    login();
+    // }
   };
 
   return (
@@ -34,12 +34,12 @@ export const Login = () => {
             <img src={LogoutIcon} width="24" />
             Logout
           </button>
-          {userNoWallet && (
+          {/* {userNoWallet && (
             <button className="btn" onClick={() => handleLogin()}>
               <img src={WalletIcon} width="18" />
               Connect Wallet
             </button>
-          )}
+          )} */}
         </>
       ) : (
         <button className="btn" onClick={() => handleLogin()}>
